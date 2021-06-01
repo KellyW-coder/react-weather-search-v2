d3e6249220144bafa89ede78a00fc9994fe1208e
KellyW-coder <79504971+KellyW-coder@users.noreply.github.com>
2021-06-01 19:54:32 +0200

    added weather component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div class="containter">
         <h1>Weather Search app</h1>
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a href="Https://www.kccd.co.za" target="_blank" rel="noreferrer">
---
+++ src/Weather.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="input-group mb-3 mt-3">
+          <input
+            className="form-control shadow-sm"
+            type="search"
+            placeholder="Search city here...."
+            autoFocus="on"
+            autoComplete="off"
+          />
+          <button className="btn btn-outline-info" type="submit" value="Search">
+            Go {`>`}
+          </button>
+          <button type="button" className="btn btn-dark" id="current-loc">
+            Current location
+          </button>
+        </div>
+      </form>
+      <h1>Cape Town</h1>
+      <ul>
+        <li>Tuesday 19:41</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly cloudy"
+          />
+          12°C
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 10%</li>
+              <li>Humidity: 82%</li>
+              <li>Wind: 15km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
